155cf7594fd019205af0c1e26017ea43db391e7c
natalimj <natalimj88@gmail.com>
2022-09-14 19:16:52 +0200

    Initial commit

---
+++ src/main/java/tric/tricproject/TricProjectApplication.java
@@ -0,0 +1,13 @@
+package tric.tricproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TricProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TricProjectApplication.class, args);
+    }
+
+}
