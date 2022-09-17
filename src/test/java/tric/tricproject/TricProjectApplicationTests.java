c64c8f8bb85a9c63bf265d6ec7ddf15ccf2df949
Bogdan Mezei <bogdan335@hotmail.com>
2022-09-17 12:12:59 +0200

    Added dependencies, plugins, changed testing plugin, disabled deployment to Azure

--- src/test/java/tric/tricproject/TricProjectApplicationTests.java
+++ src/test/java/tric/tricproject/TricProjectApplicationTests.java
@@ -1,13 +1,11 @@
 package tric.tricproject;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class TricProjectApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+    void justAnExample() {
 
+    }
 }
