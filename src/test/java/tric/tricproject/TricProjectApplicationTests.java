bd6d8a4182473917811c120a38a017006d856497
Bogdan Mezei <bogdan335@hotmail.com>
2022-09-20 17:35:33 +0200

    Failing test run

--- src/test/java/tric/tricproject/TricProjectApplicationTests.java
+++ src/test/java/tric/tricproject/TricProjectApplicationTests.java
@@ -1,11 +1,12 @@
 package tric.tricproject;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 class TricProjectApplicationTests {
 
     @Test
     void justAnExample() {
-
+        Assertions.fail();
     }
 }
