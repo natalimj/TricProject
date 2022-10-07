53a237667b5fc351886a78a77031d042f33cd44e
Bogdan Mezei <bogdan335@hotmail.com>
2022-10-07 16:49:26 +0200

    remove values

--- src/main/java/tric/tricproject/security/jwt/JwtUtils.java
+++ src/main/java/tric/tricproject/security/jwt/JwtUtils.java
@@ -14,10 +14,8 @@ import java.util.Date;
 public class JwtUtils {
 	private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-	@Value("${bezkoder.app.jwtSecret}")
 	private String jwtSecret;
 
-	@Value("${bezkoder.app.jwtExpirationMs}")
 	private int jwtExpirationMs;
 
 	public String generateJwtToken(Authentication authentication) {
