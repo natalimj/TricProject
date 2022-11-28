17a0b67d2a771dba2dd4f82a2aacdfbbc81e7702
natalimj <natalimj88@gmail.com>
2022-11-28 22:04:19 +0100

    change playInfo - add status and contributors

--- src/main/java/tric/tricproject/Model/Status.java
+++ src/main/java/tric/tricproject/Model/Status.java
@@ -2,38 +2,20 @@ package tric.tricproject.Model;
 
 import javax.persistence.*;
 
-@Entity
-@Table(name="status")
+
 public class Status {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long statusId;
-
-    @Column(name = "isActive")
     private boolean isActive;
 
-    public Status(long statusId, boolean isActive) {
-        this.statusId= statusId;
+    public Status(boolean isActive) {
         this.isActive = isActive;
-    }
-
-    public Status() {
-    }
-
-    public long getStatusId() {
-        return statusId;
-    }
-
-    public void setStatusId(long statusId) {
-        this.statusId = statusId;
     }
 
     public boolean getIsActive() {
         return isActive;
     }
 
-    public void setActive(boolean isActive) {
-        this.isActive = isActive;
+    public void setActive(boolean active) {
+        isActive = active;
     }
 }
--- src/main/java/tric/tricproject/Service/PlayInfoService.java
+++ src/main/java/tric/tricproject/Service/PlayInfoService.java
@@ -2,10 +2,14 @@ package tric.tricproject.Service;
 
 import org.springframework.stereotype.Service;
 import tric.tricproject.Model.PlayInfo;
+import tric.tricproject.Model.Status;
 
 @Service
 public interface PlayInfoService {
 
     PlayInfo editPlayInfo(PlayInfo playInfo);
     PlayInfo getPlayInfo();
+
+    PlayInfo setAppStatus(Boolean isActive);
+    Boolean getStatus();
 }
