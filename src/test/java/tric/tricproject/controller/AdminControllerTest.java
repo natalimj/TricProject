f8d89a8ed40c15eea3dc0d7fb30d0f795ee543b3
popaDaria <62464828+popaDaria@users.noreply.github.com>
2022-12-12 14:57:22 +0100

    fix controller test

--- src/test/java/tric/tricproject/controller/AdminControllerTest.java
+++ src/test/java/tric/tricproject/controller/AdminControllerTest.java
@@ -81,7 +81,7 @@ public class AdminControllerTest {
                 .andExpect(jsonPath("$.questionText").value(question.getQuestionText()))
                 .andExpect(jsonPath("$.answers.size()").value(question.getAnswers().size()))
                 .andExpect(jsonPath("$.answers[0].answerId").value(question.getAnswers().get(0).getAnswerId()))
-                .andExpect(jsonPath("$.answers[0].answerId").value(question.getAnswers().get(1).getAnswerId()))
+                .andExpect(jsonPath("$.answers[1].answerId").value(question.getAnswers().get(1).getAnswerId()))
                 .andDo(print());
 
         given(questionService.getQuestionByNumber(2)).willReturn(null);
